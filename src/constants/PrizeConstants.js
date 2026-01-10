9a1c288eb232f658d13996e3bfcd3cec38c71d2c
USER <dyfrkfl1@gmail.com>
2026-01-10 16:35:53 +0900

    feat: prize 상수 정리

---
+++ src/constants/PrizeConstants.js
@@ -0,0 +1,36 @@
+class PrizeConstants {
+  static RANK = {
+    FIRST: 1,
+    SECOND: 2,
+    THIRD: 3,
+    FOURTH: 4,
+    FIFTH: 5,
+    NONE: 0,
+  };
+
+  static MATCH_COUNT = {
+    FIRST: 5,
+    SECOND: 4,
+    THIRD: 4,
+    FOURTH: 3,
+    FIFTH: 2,
+  };
+
+  static PRIZE_AMOUNTS = {
+    1: 100000000,
+    2: 10000000,
+    3: 1500000,
+    4: 500000,
+    5: 5000,
+  };
+
+  static PROFIT_RATE_MULTIPLIER = 100;
+  static PROFIT_RATE_DECIMAL_PLACES = 1;
+}
+
+export default PrizeConstants;
+
+
+
+
+
